865c7079b97b0219b8432bd9bc70efe8e7f4b93b
Testie <testie@test.com>
2015-07-19 00:27:52 +0200

    make it better

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,20 @@
 /**
  * Created by lala on 17.07.2015.
  */
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope){
-   $scope.test = "working!";
+angular.module('app').controller('testCtrl', function($scope, $resource){
 
-    $scope.jobs = [
-        {   title: "blah1",
-            description: "blah blah"
-        },
-        {   title: "blah2",
-            description: "blah2 blah2"
-        }
-    ];
+    $scope.jobs = $resource('/api/jobs').query();
+
+    //$scope.test = "working!";
+    //
+    //$scope.jobs = [
+    //    {   title: "blah1",
+    //        description: "blah blah"
+    //    },
+    //    {   title: "blah2",
+    //        description: "blah2 blah2"
+    //    }
+    //];
 });
