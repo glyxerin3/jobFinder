d04f3b7601e5002948b12343208d60537f1a784b
Testie <testie@test.com>
2015-07-19 00:17:32 +0200

    make it better

--- models/Job.js
+++ models/Job.js
@@ -1,3 +1,23 @@
 /**
  * Created by lala on 18.07.2015.
  */
+var mongoose = require('mongoose');
+
+
+var jobSchema = mongoose.Schema({
+    title: {type:String},
+    description: {type:String}
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function() {
+  Job.find({}).exec(function(error, collection) {
+      if (collection.length === 0) {
+          Job.create({title: 'Cook', description: 'You will be making bagels'});
+          Job.create({title: 'Waiter', description: 'You will be putting food on peoples table'});
+          Job.create({title: 'Programmer', description: 'You will be mindlessly typing for hour'});
+          Job.create({title: 'Axe Maker', description: 'we need many axes made.. so many..'});
+      }
+  })
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -11,18 +12,31 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    })
+});
+
 app.get('*', function(req, res) {
     res.render('index');
 });
 
-mongoose.connect('mongodb://localhost/jobfinder');
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://marc:123456@ds047602.mongolab.com:47602/jobfinder');
+
 
 var con = mongoose.connection;
 
 con.once('open', function() {
-    console.log('connected to mongodb successfully!');
+    //console.log('connected to mongodb successfully!');
+    //console.log('running on port: ', port);
+    jobModel.seedJobs();
+    //console.log('populating empty database...');
 });
 
+var port = 3000;
+
 app.listen(process.env.PORT, process.env.IP);
-//app.listen(3000);
+//app.listen(port);
 
