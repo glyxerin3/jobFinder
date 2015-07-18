2d1329bb645e4a69229272d51e9cb25568cb5170
Testie <testie@test.com>
2015-07-18 23:31:33 +0200

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Created by lala on 16.07.2015.
  */
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -14,6 +15,14 @@ app.get('*', function(req, res) {
     res.render('index');
 });
 
-app.listen(process.env.PORT, process.env.IP);
-//app.listen(3000);
+mongoose.connect('mongodb://localhost/jobfinder');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!');
+});
+
+//app.listen(process.env.PORT, process.env.IP);
+app.listen(3000);
 
